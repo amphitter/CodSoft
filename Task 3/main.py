1dee2dcdde0cb07ecb645538f5e0bc866b718ef7
Devansh Singh <145259402+amphitter@users.noreply.github.com>
2024-06-06 18:40:00 +0530

    Add files via upload

---
+++ Task 3/main.py
@@ -0,0 +1,24 @@
+import string
+import random
+
+def gen_pass(length):
+    if length < 1:
+        return "Password should be of at least 1 length"
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    print("Password Generator")
+    length = int(input("Enter the desired length of password: "))
+    if length <= 0:
+        print("The length should be a positive integer.")
+    else:
+        password = gen_pass(length)
+        print(f"Generated Password: {password}")
+
+if __name__ == "__main__":
+    main()
+
+    
+            
